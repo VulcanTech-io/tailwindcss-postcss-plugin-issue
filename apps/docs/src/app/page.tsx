71363ffa3b8e55207cfd507f7685cbac9c03ffaf
VulcanTech-io <vulcantechnologies.io@gmail.com>
2025-03-05 07:08:01 -0700

    changes

---
+++ apps/docs/src/app/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Milkdown, MilkdownProvider, useEditor } from "@milkdown/react";
+import { Crepe } from "@milkdown/crepe";
+// import "@milkdown/crepe/theme/common/style.css";
+// import "@milkdown/crepe/theme/frame.css";
+import "./tailwind.css";
+const markdown = `
+  # Title Section
+  ## sub header
+  ### sub sub header
+  Content . . .
+`;
+
+export const MilkdownEditor: React.FC = () => {
+  useEditor((root) => {
+    const crepe = new Crepe({
+      root,
+      defaultValue: markdown,
+    });
+    return crepe;
+  }, []);
+
+  // @ts-ignore
+  return <Milkdown />;
+};
+
+export default function ArticleEditor() {
+  return (
+    <div className="mt-16 sm:mt-32">
+      <div className="bg-white/95 text-blue-400">
+        hello
+        {/* @ts-ignore */}
+        <MilkdownProvider>
+          <MilkdownEditor />
+        </MilkdownProvider>
+      </div>
+    </div>
+  );
+}
